903390c5f58c028454eb3598035fe4aca32542aa
JonathanSaragih <jonathan.srgh@gmail.com>
2025-05-30 22:29:13 +0700

    fix lstm

--- src/lstm/save_lstm_data.py
+++ src/lstm/save_lstm_data.py
@@ -1,30 +1,19 @@
-"""
-Helper script to save LSTM model data for reuse in the from_scratch implementation.
-Run this after training the model in lstm_keras_training.ipynb to save the
-necessary data that can be imported by other Python scripts.
-"""
-
 import tensorflow as tf
 import numpy as np
 import pickle
 import os
 import sys
 
-# Add path to import from the notebook
 sys.path.insert(0, os.path.dirname(__file__))
 
-# Try to import variables from the keras training
 try:
-    # Get global variables from the notebook using notebook's globals
     from lstm_keras_training import (
         test_sequences, test_labels, vectorizer, 
         best_result, build_lstm_model
     )
     
-    # Extract best config
     best_config = best_result['config']
     
-    # Save the data
     data_to_save = {
         'test_sequences': test_sequences,
         'test_labels': test_labels,
